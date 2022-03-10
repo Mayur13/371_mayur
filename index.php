84bdd58747f918d8569244e10b26d129a5310b5f
Mayur13 <62945684+Mayur13@users.noreply.github.com>
2022-03-10 19:48:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 
 //header('Location: /index.html); 
-echo "Hello world";
+echo "Hello world !!! This is just the deployement of basic Html file using Heroku as Platform as a servive.";
 
 ?>
